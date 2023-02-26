f39b347472d35f8ebc9425f3848a610061271be3
zhuo18329751762 <2199698862@qq.com>
2023-02-26 20:32:51 +0800

    Mybaits

---
+++ mybaits-Demo/src/main/java/Demo/MyBatisDemo1.java
@@ -0,0 +1,32 @@
+package Demo;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import pojo.User;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/*
+mybatis快速入门
+ */
+public class MyBatisDemo1 {
+    public static void main(String[] args) throws IOException {
+        // 1 加载mybatis的核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        // 2 获取sqlSession对象，用来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 3 执行sql
+        List<User> users = sqlSession.selectList("test.selectAll");
+        System.out.println(users);
+        // 4 释放资源
+        sqlSession.close();
+    }
+}
