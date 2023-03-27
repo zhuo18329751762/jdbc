3773796528bd97bdc419747e84a2f527bf1ae361
zhuo18329751762 <2199698862@qq.com>
2023-03-27 20:39:58 +0800

    vue

---
+++ ajax-demo/src/main/java/com/yangzhuo/json/FastJsonDemo.java
@@ -0,0 +1,18 @@
+package com.yangzhuo.json;
+
+import com.alibaba.fastjson.JSON;
+
+public class FastJsonDemo {
+    public static void main(String[] args) {
+        // 1 将Java对象转化为JSON字符串
+        User user=new User();
+        user.setId(1);
+        user.setUsername("zhangsan");
+        user.setPassword("123");
+        String s = JSON.toJSONString(user);
+        System.out.println(s);//{"id":1,"password":"123","username":"zhangsan"}
+        // 2 将JSON字符串转为Java对象
+        User u = JSON.parseObject("{\"id\":1,\"password\":\"123\",\"username\":\"zhangsan\"}", User.class);
+        System.out.println(u);
+    }
+}
